8b83244e23296814d79965c07b439921690ca2ae
mashrufahemd <mashrufahemd95@gmail.com>
2025-12-14 19:32:13 +0600

    chang origin

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.ORIGIN_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   })
